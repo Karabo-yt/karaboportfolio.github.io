cf5650a9e6385cc191b3e6d71d5685596c0ed78e
CYBER_KARABO <karaboemma25@gmail.com>
2025-09-01 20:14:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -8,5 +8,10 @@ menuToggle.addEventListener('click', () =>{
 });
 
 document.getElementById("myButton").addEventListener("click", function () {
-  alert("WELCOME TO MY PROTOTYPE WEBSITE,INJECTING DEVICE WITH VIRUS IN -10");
+  alert("CONTACT ME FOR MORE INFO, INJECTING DEVICE WITH VIRUS IN T -10....");
 });
+
+
+  document.body.style.backgroundImage = "url('https://www.bing.com/images/search?view=detailV2&ccid=y3Xo2MMx&id=F3E51C97B2317AF904C8A1C73C849FA3D172AC4C&thid=OIP.y3Xo2MMxOSxxmsK1ndgaNQHaEK&mediaurl=https%3a%2f%2fwallpapercave.com%2fwp%2fwp6079777.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.cb75e8d8c331392c719ac2b59dd81a35%3frik%3dTKxy0aOfhDzHoQ%26pid%3dImgRaw%26r%3d0&exph=1080&expw=1920&q=Aesthetic+Wallpaper+Dektop+Neon&FORM=IRPRST&ck=9A3CA368B6A481B883E998815E652574&selectedIndex=2&itb=0')";
+  document.body.style.backgroundSize = "cover";
+  document.body.style.backgroundPosition = "center";
